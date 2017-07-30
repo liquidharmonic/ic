a0992cd9645bb1fd6aa10c9a0d982828242f18b6
liquidharmonic <eric.ko.9000@gmail.com>
2017-07-29 22:55:58 -0400

    finish event worker spec

--- spec/workers/event_worker_spec.rb
+++ spec/workers/event_worker_spec.rb
@@ -1,4 +1,22 @@
 require 'rails_helper'
-RSpec.describe EventWorker, type: :worker do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe EventWorker, type: :worker do
+  describe '#perform' do
+    it 'should create Event' do
+      Sidekiq::Testing.inline! do
+        expect{
+          EventWorker.perform_async('123', 'action')
+        }.to change{Event.count}.by(1)
+      end
+      event = Event.last
+      expect(event.annonymous_key).to eql '123'
+      expect(event.action).to eql 'action'
+    end
+    it 'should raise error if it cannot create Event' do
+      Sidekiq::Testing.inline! do
+        expect{
+          EventWorker.perform_async(nil, nil)
+        }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+  end
 end
