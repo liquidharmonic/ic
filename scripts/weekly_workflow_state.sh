92166d4ee0dcff6e5bdcf52404e89a532492c60c
liquidharmonic <eric.ko.9000@gmail.com>
2017-07-29 23:18:02 -0400

    add comments in the script

--- scripts/weekly_workflow_state.sh
+++ scripts/weekly_workflow_state.sh
@@ -1,3 +1,9 @@
+# Requirements
+# This script requires to be in the provided rails project (https://github.com/liquidharmonic/ic) in the scripts dir.
+# The bash script simply invokes the rake task which performs the query. I did this so I could easily test it's functionality using rspec.
+# You can call the rake task directly by running
+# `rails "analytics:weekly_workflow_state[<start_date>, <end date>]"`
+
 if [ -z "$1" ] || [ -z "$2" ] ; then
   echo "You need to pass in start and end date in 'YYYY-mm-dd' format"
   exit 1
